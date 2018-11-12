35fae9585012dcb4b5d959f6dbf96af879ed14e7
Sam Holmes <samholmes1337@gmail.com>
2018-11-12 05:06:49 +0000

    Heavy syntax refactor

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const _ = require("lodash");
 
+/**
+ * The default options for the package. These are defined and configured properly
+ * in the vue.config.js file under "pluginOptions.svg".
+ * @type {Object}
+ */
 const defaults = {
 	inline: { resourceQuery: /inline/, svgo: false }, // Options for vue-svg-loader
 	sprite: { resourceQuery: /sprite/, extract: false }, // Options for svg-sprite-loader
@@ -12,10 +17,10 @@ const defaults = {
  * @param  {PluginAPI} api     An instance of the PluginAPI
  * @param  {Object}    options All options specified in vue.config.js
  */
-function handler(api, options) {
+module.exports = function handler(api, options) {
 	options = parseOptions(options);
 	api.chainWebpack(config => setup(config, options));
-}
+};
 
 /**
  * Perform the main setup of the svg rule parsing and rewriting. This uses the
@@ -26,18 +31,17 @@ function handler(api, options) {
 function setup(config, options) {
 	const rule = config.module.rule("svg"); // Find the svg rule
 
+	/*
+	 * Let's use the file loader option defaults for the svg loader again. Otherwise
+	 * we'll have to set our own and it's no longer consistent with the changing
+	 * vue-cli.
+	 */
 	const fileLoaderOptions = rule.use("file-loader").get("options"); // Get the file loader options
 	options.external = _.merge(fileLoaderOptions, options.external); // Make sure we save the file loader options
 	options.sprite = _.merge({ spriteFilename: fileLoaderOptions.name }, options.sprite); // Use file loader options for sprite name
 
 	rule.uses.clear(); // Clear out existing uses of the svg rule
-
-	const query = {}; // Keep track of the resource queries
-	for (option in options) {
-		if (!options[option].resourceQuery) continue; // Skip if no query
-		query[option] = options[option].resourceQuery; // Get the query
-		delete options[option].resourceQuery; // Delete the field (to prevent passing it as a loader option)
-	}
+	const query = parseResourceQuery(options); // Get the resource queries
 
 	rule
 		.oneOf("inline").resourceQuery(query.inline).use("vue-svg-loader").loader("vue-svg-loader").options(options.inline).end().end()
@@ -48,6 +52,7 @@ function setup(config, options) {
 
 function parseOptions({ pluginOptions = {} }) {
 	let { svg } = pluginOptions; // Get the svg configuration
+
 	if (typeof svg === "object") svg = _.merge(defaults, svg); // If exists, merge with defaults
 	else if (!svg) svg = defaults; // If no config set, use defaults
 	else throw new TypeError(`pluginOptions.svg is of type "${typeof svg}", which is not a valid configuration type`);
@@ -55,4 +60,20 @@ function parseOptions({ pluginOptions = {} }) {
 	return svg;
 }
 
-module.exports = handler;
+/**
+ * Processes the options object passed to the app by extracting the resource query
+ * options and returning them so that they can be used later.
+ * @param  {object} options The plugin options object
+ * @return {object}         An object containing the resource queries and their relevant config names
+ */
+function parseResourceQuery(options) {
+	const query = {};
+
+	for (option in options) {
+		if (!options[option].resourceQuery) continue; // Skip if no query
+		query[option] = options[option].resourceQuery; // Get the query
+		delete options[option].resourceQuery; // Delete the field (to prevent passing it as a loader option)
+	}
+
+	return query;
+}
